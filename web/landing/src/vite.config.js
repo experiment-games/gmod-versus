ca04736eaa554409a47e02d0ed972e7614969d1e
luttje <2738114+luttje@users.noreply.github.com>
2026-03-01 14:44:14 +0100

    Simplify build by using public for landing page

--- web/landing/src/vite.config.js
+++ web/landing/src/vite.config.js
@@ -1,22 +1,12 @@
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: './',
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'assets/in-game/*.{mp4,jpg,png}',
-          dest: 'assets/in-game',
-        },
-      ],
-    }),
-  ],
+  plugins: [],
   build: {
     outDir: resolve(__dirname, '../dist'),
     emptyOutDir: true,
