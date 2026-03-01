52821e76814601582687cb1a9cc69dc95d5686c7
luttje <2738114+luttje@users.noreply.github.com>
2026-03-01 19:24:42 +0100

    exp c18 v1 033 is actually on workshop

--- tools/server/start_server.sh
+++ tools/server/start_server.sh
@@ -25,7 +25,7 @@ cmd="/home/steam/server_1/srcds_run -console \
         -tickrate 100 \
         +maxplayers 64 \
         +gamemode versus \
-        +map exp_c18_v1_alpha034 \
+        +map exp_c18_v1_alpha033 \
         +host_workshop_collection 3674693854 \
         +sv_setsteamaccount REPLACE_WITH_STEAM_API_KEY"
 
