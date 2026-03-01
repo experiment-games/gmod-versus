de7ac7fa662cd23e607e172c6b8e9c3d1d0e2ea1
luttje <2738114+luttje@users.noreply.github.com>
2026-03-01 14:05:55 +0100

    Add media to landing webpage

--- web/landing/src/vite.config.js
+++ web/landing/src/vite.config.js
@@ -1,11 +1,22 @@
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: './',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'assets/in-game/*.mp4',
+          dest: 'assets/in-game',
+        },
+      ],
+    }),
+  ],
   build: {
     outDir: resolve(__dirname, '../dist'),
     emptyOutDir: true,
