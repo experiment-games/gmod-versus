a5b7cd9f8b73eae93bcd2a38e003192810ecc8f2
luttje <2738114+luttje@users.noreply.github.com>
2026-03-08 21:02:20 +0100

    Add chef's hat

---
+++ addon/gamemodes/versus/gamemode/plugins/cosmetics/items/sh_hat_chef.lua
@@ -0,0 +1,90 @@
+local PLUGN = PLUGN
+local ITEM = ITEM
+
+ITEM.base = "base_equipment"
+ITEM.name = "Chef's Hat"
+ITEM.category = "Clothing (Hat)"
+ITEM.size = 0
+ITEM.cost = 4000
+ITEM.equipSlot = "hat"
+ITEM.model = "models/chefhat.mdl"
+ITEM.description = "You can't be a master chef without the proper headwear!"
+ITEM.lootWeight = 2.5 / 100
+
+function ITEM:onDrop(player, position) end
+
+function ITEM:getPacData(player, entity)
+  local size = 1
+  local scale = Vector(1.25, 1, 1)
+  local angles = Angle(-5.8835282325745, 112.89134979248, 90.873680114746)
+  local position = Vector(2.5086135864258, 0.2825927734375, 0.1539306640625)
+
+  if (not entity:GetModel():find("female")) then
+    size = 0.98
+  end
+
+  return {
+    [1] = {
+      ["children"] = {
+        [1] = {
+          ["children"] = {
+          },
+          ["self"] = {
+            ["Skin"] = 0,
+            ["UniqueID"] = "4bf1e2f076a883decdfc3f1014c4fab953c56a758bd254287f11ac12b680a402",
+            ["NoLighting"] = false,
+            ["AimPartName"] = "",
+            ["IgnoreZ"] = false,
+            ["AimPartUID"] = "",
+            ["Notes"] = "",
+            ["Materials"] = "",
+            ["Name"] = "",
+            ["LevelOfDetail"] = 0,
+            ["NoTextureFiltering"] = false,
+            ["PositionOffset"] = Vector(-0.10000000149012, 0, 0),
+            ["IsDisturbing"] = false,
+            ["EyeAngles"] = false,
+            ["DrawOrder"] = 0,
+            ["TargetEntityUID"] = "",
+            ["Alpha"] = 1,
+            ["Material"] = "",
+            ["Invert"] = false,
+            ["ForceObjUrl"] = false,
+            ["Bone"] = "head",
+            ["Color"] = Vector(1, 1, 1),
+            ["AngleOffset"] = Angle(-0.30000001192093, 0, 0),
+            ["BoneMerge"] = false,
+            ["Angles"] = angles,
+            ["Position"] = position,
+            ["ClassName"] = "model2",
+            ["NoCulling"] = false,
+            ["Hide"] = false,
+            ["Brightness"] = 1,
+            ["Scale"] = scale,
+            ["LegacyTransform"] = false,
+            ["EditorExpand"] = false,
+            ["Size"] = size,
+            ["Translucent"] = false,
+            ["BlendMode"] = "",
+            ["ModelModifiers"] = "",
+            ["EyeTargetUID"] = "",
+            ["Model"] = "models/chefhat.mdl",
+          },
+        },
+      },
+      ["self"] = {
+        ["DrawOrder"] = 0,
+        ["UniqueID"] = "214e270be69be23e5665bb498bfdf5252d6af487eacc8f3205d347153c66545c",
+        ["Notes"] = "",
+        ["Hide"] = false,
+        ["Name"] = "my outfit",
+        ["TargetEntityUID"] = "",
+        ["EditorExpand"] = true,
+        ["OwnerName"] = "self",
+        ["Duplicate"] = false,
+        ["IsDisturbing"] = false,
+        ["ClassName"] = "group",
+      },
+    },
+  }
+end
