44b79722f0d7c0ba268292423788a1a32a0cf81b
luttje <2738114+luttje@users.noreply.github.com>
2026-03-18 16:40:15 +0100

    Link to discord on join (closes #186)

---
+++ addon/gamemodes/versus/gamemode/plugins/discord/sv_init.lua
@@ -0,0 +1,13 @@
+local PLUGIN = PLUGIN
+
+function PLUGIN.hook:PlayerInitialSpawn(player)
+  timer.Simple(1, function()
+    if IsValid(player) then
+      versus.messages.notify(
+        player,
+        "Welcome! Join our Discord for support, news, and more! https://discord.gg/U4x4HgpNhy",
+        NOTIFY_CHAT_LIGHTBULB
+      )
+    end
+  end)
+end
