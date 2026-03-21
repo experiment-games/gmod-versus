7748cb44efe80bf9b42971e495a4b3f6cbf07293
luttje <2738114+luttje@users.noreply.github.com>
2026-03-21 07:32:11 +0100

    Fix typo

--- addon/gamemodes/versus/gamemode/plugins/discord/sv_init.lua
+++ addon/gamemodes/versus/gamemode/plugins/discord/sv_init.lua
@@ -3,7 +3,7 @@ local PLUGIN = PLUGIN
 function PLUGIN.hook:PlayerInitialSpawn(player)
   timer.Simple(1, function()
     if IsValid(player) then
-      versus.messages.notify(
+      versus.message.notify(
         player,
         "Welcome! Join our Discord for support, news, and more! https://discord.gg/U4x4HgpNhy",
         NOTIFY_CHAT_LIGHTBULB
